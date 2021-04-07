8c4eaf32feb5af1abecf78bf7accf1db42143e9b
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-07 18:23:57 +0800

    feat: add APIClasses

---
+++ APIClasses/Math/AllMath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace APIClasses.Math
+{
+    //TODO - move all of these out into separate classes
+    public class MathResult
+    {
+        public int Result;
+
+        public MathResult(int result)
+        {
+            Result = result;
+        }
+    }
+
+    public class BooleanResult
+    {
+        public bool Result;
+
+        public BooleanResult(bool result)
+        {
+            Result = result;
+        }
+    }
+
+    public class OneValueInput
+    {
+        public int Value;
+    }
+
+    public class TwoValueInput
+    {
+        public int Value1;
+        public int Value2;
+    }
+    
+    public class ThreeValueInput
+    {
+        public int Value1;
+        public int Value2;
+        public int Value3;
+    }
+}
