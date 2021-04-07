e5049c4c2dd80a2ae9da7fa34616f983a439f201
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-07 22:01:52 +0800

    feat: add ServerInterfaceLib

---
+++ ServerInterfaceLib/IAuthenticationServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerInterfaceLib
+{
+    [ServiceContract]
+    public interface IAuthenticationServer
+    {
+        [OperationContract]
+        string Register(string name, string password);
+
+        [OperationContract]
+        int Login(string name, string password);
+
+        [OperationContract]
+        string Validate(int token);            
+    }
+}
