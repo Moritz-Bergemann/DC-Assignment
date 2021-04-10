2198e28137024bac356b38544c0f70ef7b048c0b
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-10 16:54:15 +0800

    feat: Update APIClasses with registry data

---
+++ APIClasses/Registry/AllRegistry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClasses.Registry
+{
+    public class RegistryData
+    {
+        public string Name;
+        public string Description;
+        public string ApiEndpoint;
+        public int NumOperands;
+        public string OperandType;
+    }
+
+    public class EndpointData
+    {
+        public string Endpoint;
+    }
+}
