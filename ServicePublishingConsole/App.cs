3d505d611567f8169ec598a71983643c2229302d
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-11 16:14:37 +0800

    feat: Add publish & unpublish to ServicePublishingConsole

--- ServicePublishingConsole/App.cs
+++ ServicePublishingConsole/App.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using APIClasses.Registry;
+using Newtonsoft.Json;
+using RestSharp;
 using ServerInterfaceLib;
 
 namespace ServicePublishingConsole
@@ -13,15 +17,19 @@ namespace ServicePublishingConsole
         //User login token
         private int _token;
         private IAuthenticationServer _authServer;
+        private RestClient _registryClient;
 
-        public App(string url)
+        public App(string authUrl, string registryUrl)
         {
             //Get the data server
             NetTcpBinding tcp = new NetTcpBinding();
 
-            //Create connection factory for connection to server
-            ChannelFactory<ServerInterfaceLib.IAuthenticationServer> serverChannelFactory = new ChannelFactory<IAuthenticationServer>(tcp, url);
+            //Create connection factory for connection to auth server
+            ChannelFactory<IAuthenticationServer> serverChannelFactory = new ChannelFactory<IAuthenticationServer>(tcp, authUrl);
             _authServer = serverChannelFactory.CreateChannel();
+
+            //Setup registry server client
+            _registryClient = new RestClient(registryUrl);
 
             _token = -1;
         }
@@ -31,40 +39,47 @@ namespace ServicePublishingConsole
             try
             {
                 bool run = true;
-
-                string introText = "Service publishing command line program.\n" +
-                                   "Enter 'help' for command information.";
-                Console.WriteLine(introText);
+                
+                Console.WriteLine("Service publishing command line program.\n" +
+                                  "Enter 'help' for command information.");
 
                 while (run)
                 {
-                    Console.Write(">");
-                    string[] userInputElements = Console.ReadLine().Split(' ');
-                    switch (userInputElements[0])
+                    try
                     {
-                        case "help":
-                            Help();
-                            break;
-                        case "register":
-                            Register();
-                            //new ArraySegment<string>(userInputElements, 1, userInputElements.Length - 1).Array //TODO remove this
-                            break;
-                        case "login":
-                            Login();
-                            //new ArraySegment<string>(userInputElements, 1, userInputElements.Length - 1).Array //TODO remove this
-                            break;
-                        case "publish":
-                            Publish();
-                            break;
-                        case "unpublish":
-                            Unpublish();
-                            break;
-                        case "exit":
-                            run = false;
-                            break;
-                        default:
-                            Console.WriteLine($"Unknown option '{userInputElements[0]}' - enter 'help' for help");
-                            break;
+                        Console.Write(">");
+                        string[] userInputElements = Console.ReadLine().Split(' ');
+                        switch (userInputElements[0])
+                        {
+                            case "help":
+                                Help();
+                                break;
+                            case "register":
+                                Register();
+                                //new ArraySegment<string>(userInputElements, 1, userInputElements.Length - 1).Array //TODO remove this
+                                break;
+                            case "login":
+                                Login();
+                                //new ArraySegment<string>(userInputElements, 1, userInputElements.Length - 1).Array //TODO remove this
+                                break;
+                            case "publish":
+                                Publish();
+                                break;
+                            case "unpublish":
+                                Unpublish();
+                                break;
+                            case "exit":
+                                run = false;
+                                break;
+                            default:
+                                Console.WriteLine($"Unknown option '{userInputElements[0]}' - enter 'help' for help");
+                                break;
+                        }
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Bad input.");
                     }
                 }
 
@@ -78,15 +93,13 @@ namespace ServicePublishingConsole
 
         private static void Help()
         {
-            string helpString =
-                "This application allows you to publish services using the networked service publisher.\n" +
-                "Command List:" +
-                "\nhelp - This output. \n" +
-                "\tregister - Register a new user with the authentication service. \n" +
-                "\tlogin - Get a new session token using your user credentials that allows you to interact with the publishing database. \n" +
-                "\tpublish - TBA \n" + //TODO
-                "\tunpublish - TBA"; //TODO
-            Console.WriteLine(helpString);
+            Console.WriteLine("This application allows you to publish services using the networked service publisher.\n" +
+                              "Command List:" +
+                              "\nhelp - This output. \n" +
+                              "\tregister - Register a new user with the authentication service. \n" +
+                              "\tlogin - Get a new session token using your user credentials that allows you to interact with the publishing database. \n" +
+                              "\tpublish - Publish a service to the registry server (Requires authorisation). \n" +
+                              "\tunpublish - Remove a service from the registry server by API endpoint (Requires authorisation).");
         }
 
         private void Register()
@@ -137,18 +150,77 @@ namespace ServicePublishingConsole
 
         private void Publish()
         {
-            Console.WriteLine("TBA"); //TODO
+            try
+            {
+                //Prepare registry data
+                RegistryData data = new RegistryData();
+
+                Console.Write("Service name: ");
+                data.Name = Console.ReadLine();
+                Console.Write("Service description: ");
+                data.Description = Console.ReadLine();
+                Console.Write("Service API endpoint: ");
+                data.ApiEndpoint = Console.ReadLine();
+                Console.Write("Number of operands: ");
+                data.NumOperands = int.Parse(Console.ReadLine());
+                Console.Write("Operand type: ");
+                data.OperandType = Console.ReadLine();
+
+                //Attempt publish on server
+                RestRequest request = new RestRequest("api/publish");
+                request.AddJsonBody(data);
+                IRestResponse response = _registryClient.Post(request);
+                PublishResult result = JsonConvert.DeserializeObject<PublishResult>(response.Content);
+
+                Console.WriteLine($"Publish {(result.Success ? "succeeded" : "failed")} - {result.Message}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Could not parse input. Aborted.");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Bad input. Aborted.");
+            }
         }
 
         private void Unpublish()
         {
-            Console.WriteLine("TBA"); //TODO
+            try
+            {
+                //Prepare registry data
+                EndpointData data = new EndpointData();
+
+                Console.Write("API endpoint to unpublish: ");
+                data.ApiEndpoint = Console.ReadLine();
+                
+                //Attempt publish on server
+                RestRequest request = new RestRequest("api/unpublish");
+                request.AddJsonBody(data);
+                IRestResponse response = _registryClient.Post(request);
+                PublishResult result = JsonConvert.DeserializeObject<PublishResult>(response.Content);
+
+                Console.WriteLine($"Unpublish {(result.Success ? "succeeded" : "failed")} - {result.Message}");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Bad input. Aborted.");
+            }
         }
 
         static void Main(string[] args)
         {
-            string url = "net.tcp://localhost:8101/AuthenticationProvider";
-            App app = new App(url);
+            //Intercept keyboard interrupt
+            Console.CancelKeyPress += delegate
+            {
+                Console.WriteLine();
+                Console.WriteLine("Keyboard Interrupt - Goodbye");
+                Environment.Exit(0);
+            };
+
+            string authUrl = "net.tcp://localhost:8101/AuthenticationProvider";
+            string registryUrl = "https://localhost:44330/";
+            App app = new App(authUrl, registryUrl);
 
             app.Run();
         }
