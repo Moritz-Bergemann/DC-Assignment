533ef6faee384067a824a8ea4c992e0a48bedfe2
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-13 21:55:58 +0800

    docs: commenting

--- ClientGUI/UsernamePasswordUserControl.xaml.cs
+++ ClientGUI/UsernamePasswordUserControl.xaml.cs
@@ -17,18 +17,25 @@ namespace ClientGUI
 {
     /// <summary>
     /// Interaction logic for UsernamePasswordUserControl.xaml
+    /// UserControl for inputting username and password and performing action based on these.
     /// </summary>
     public partial class UsernamePasswordUserControl : UserControl
     {
         //Action to perform upon login
         private readonly Action<string, string> _confirmAction;
 
-        private bool _closeOnExit;
+        private readonly bool _closeOnConfirm;
 
-        public UsernamePasswordUserControl(string prompt, Action<string, string> confirmAction, bool closeOnExit)
+        /// <summary>
+        /// Constructor for UserControl.
+        /// </summary>
+        /// <param name="prompt">Prompt string to display before input</param>
+        /// <param name="confirmAction">Function to perform with username and password upon confirmation</param>
+        /// <param name="closeOnConfirm">Whether the current window should be closed upon pressing "confirm"</param>
+        public UsernamePasswordUserControl(string prompt, Action<string, string> confirmAction, bool closeOnConfirm)
         {
             _confirmAction = confirmAction;
-            _closeOnExit = closeOnExit;
+            _closeOnConfirm = closeOnConfirm;
 
             InitializeComponent();
 
@@ -42,7 +49,7 @@ namespace ClientGUI
                 //Run action in delegator to run with username & password data
                 _confirmAction(UsernameInput.Text, PasswordInput.Text);
 
-                if (_closeOnExit)
+                if (_closeOnConfirm)
                 {
                     Window.GetWindow(this)?.Close();
                 }
