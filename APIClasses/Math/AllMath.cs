25edf27c7aafae01fb316fbaed15f0f81cfc0a8d
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-18 14:27:17 +0800

    feat: Switch to single struct for all result returns

--- APIClasses/Math/AllMath.cs
+++ APIClasses/Math/AllMath.cs
@@ -4,70 +4,110 @@ using System.Runtime.InteropServices;
 namespace APIClasses.Math
 {
     //TODO - move all of these out into separate classes
-    public class MathResult
+    public class MathResult 
     {
-        public int Result;
+        public string Result;
         public bool Success;
         public string Message;
 
         public MathResult(int result)
         {
+            Result = result.ToString();
+            Success = true;
+            Message = "success";
+        }
+
+        public MathResult(List<int> result)
+        {
+            Result = result.ToString();
+            Success = true;
+            Message = "success";
+        }
+
+        public MathResult(bool result)
+        {
+            Result = result.ToString();
+            Success = true;
+            Message = "success";
+        }
+
+        public MathResult(string result)
+        {
             Result = result;
             Success = true;
             Message = "success";
         }
 
-        public MathResult(int result, bool success, string message)
+        public MathResult(string result, bool success, string message)
         {
             Result = result;
             Success = success;
             Message = message;
         }
-
     }
-
-    public class MathListResult
-    {
-        public List<int> Results;
-        public bool Success;
-        public string Message;
-
-        public MathListResult(List<int> results, bool success, string message)
-        {
-            Results = results;
-            Success = success;
-            Message = message;
-        }
-        public MathListResult(List<int> results)
-        {
-            Results = results;
-            Success = true;
-            Message = "success";
-        }
-    }
-
-    public class MathBooleanResult
-    {
-        public bool Result;
-        public bool Success;
-        public string Message;
-
-        public MathBooleanResult(bool result, bool success, string message)
-        {
-            Result = result;
-            Success = success;
-            Message = message;
-        }
-        public MathBooleanResult(bool result)
-        {
-            Result = result;
-            Success = true;
-            Message = "success";
-        }
-    }
-
     public class MathInput
     {
         public List<int> Values = new List<int>();
     }
+
+    //public class MathResult
+    //{
+    //    public int Result;
+    //    public bool Success;
+    //    public string Message;
+
+    //    public MathResult(int result)
+    //    {
+    //        Result = result;
+    //        Success = true;
+    //        Message = "success";
+    //    }
+
+    //    public MathResult(int result, bool success, string message)
+    //    {
+    //        Result = result;
+    //        Success = success;
+    //        Message = message;
+    //    }
+    //}
+
+    //public class MathListResult
+    //{
+    //    public List<int> Results;
+    //    public bool Success;
+    //    public string Message;
+
+    //    public MathListResult(List<int> results, bool success, string message)
+    //    {
+    //        Results = results;
+    //        Success = success;
+    //        Message = message;
+    //    }
+    //    public MathListResult(List<int> results)
+    //    {
+    //        Results = results;
+    //        Success = true;
+    //        Message = "success";
+    //    }
+    //}
+
+    //public class MathBooleanResult
+    //{
+    //    public bool Result;
+    //    public bool Success;
+    //    public string Message;
+
+    //    public MathBooleanResult(bool result, bool success, string message)
+    //    {
+    //        Result = result;
+    //        Success = success;
+    //        Message = message;
+    //    }
+    //    public MathBooleanResult(bool result)
+    //    {
+    //        Result = result;
+    //        Success = true;
+    //        Message = "success";
+    //    }
+    //}
 }
--- ServiceProvider/Controllers/MathController.cs
+++ ServiceProvider/Controllers/MathController.cs
@@ -12,7 +12,7 @@ namespace ServiceProvider.Controllers
         public MathResult AddTwoNumbers(MathInput input)
         {
             if (input.Values.Count != 2)
-                return new MathResult(-1, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
             return new MathResult(input.Values[0] + input.Values[1]); 
         }
@@ -22,7 +22,7 @@ namespace ServiceProvider.Controllers
         public MathResult AddThreeNumbers(MathInput input)
         {
             if (input.Values.Count != 3)
-                return new MathResult(-1, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
             return new MathResult(input.Values[0] + input.Values[1] + input.Values[2]);
         }
@@ -32,7 +32,7 @@ namespace ServiceProvider.Controllers
         public MathResult MultiplyTwoNumbers(MathInput input)
         {
             if (input.Values.Count != 2)
-                return new MathResult(-1, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
             return new MathResult(input.Values[0] * input.Values[1]);
         }
@@ -42,53 +42,53 @@ namespace ServiceProvider.Controllers
         public MathResult MultiplyThreeNumbers(MathInput input)
         {
             if (input.Values.Count != 3)
-                return new MathResult(-1, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
             return new MathResult(input.Values[0] * input.Values[1] * input.Values[2]);
         }
 
         [Route("api/prime-to")]
         [HttpPost]
-        public MathListResult GetPrimesTo(MathInput input)
+        public MathResult GetPrimesTo(MathInput input)
         {
             if (input.Values.Count != 1)
-                return new MathListResult(null, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
             try
             {
-                return new MathListResult(MathModel.GeneratePrimesUpTo(input.Values[0])); //TODO catch ArgumentException
+                return new MathResult(MathModel.GeneratePrimesUpTo(input.Values[0])); //TODO catch ArgumentException
             }
             catch (ArgumentException a)
             {
-                return new MathListResult(null, false, "calculation error: " + a.Message);
+                return new MathResult(null, false, "calculation error: " + a.Message);
             }
         }
 
         [Route("api/prime-range")]
         [HttpPost]
-        public MathListResult GetPrimesInRange(MathInput input)
+        public MathResult GetPrimesInRange(MathInput input)
         {
             if (input.Values.Count != 1)
-                return new MathListResult(null, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
             try
             {
-                return new MathListResult(MathModel.GeneratePrimesBetween(input.Values[0], input.Values[1])); //TODO catch ArgumentException
+                return new MathResult(MathModel.GeneratePrimesBetween(input.Values[0], input.Values[1])); //TODO catch ArgumentException
             }
             catch (ArgumentException a)
             {
-                return new MathListResult(null, false, "calculation error: " + a.Message);
+                return new MathResult(null, false, "calculation error: " + a.Message);
             }
         }
 
         [Route("api/is-prime")]
         [HttpPost]
-        public MathBooleanResult IsPrimeNumber(MathInput input)
+        public MathResult IsPrimeNumber(MathInput input)
         {
             if (input.Values.Count != 1)
-                return new MathBooleanResult(false, false, "bad number of parameters");
+                return new MathResult(null, false, "bad number of parameters");
 
-            return new MathBooleanResult(MathModel.IsPrime(input.Values[0]));
+            return new MathResult(MathModel.IsPrime(input.Values[0]));
         }
     }
 }
