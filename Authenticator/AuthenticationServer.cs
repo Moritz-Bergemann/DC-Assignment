7e2c9a9e22a05e14113de0138fb20018ccf3f8f5
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-23 13:48:38 +0800

    feat: Add periodic clear for tokens

--- Authenticator/AuthenticationServer.cs
+++ Authenticator/AuthenticationServer.cs
@@ -2,23 +2,27 @@
 using System;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.ServiceModel;
+using System.Threading;
 
 namespace Authenticator
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
     class AuthenticationServer : IAuthenticationServer
     {
-        private readonly string accountsPath;
-        private readonly string tokensPath;
+        private readonly string _accountsPath;
+        private readonly string _tokensPath;
 
-        private readonly Random random;
+        private readonly Random _random;
+        private Timer _tokenClearer;
 
         private AuthenticationServer(string accountsPath, string tokensPath)
         {
-            this.accountsPath = accountsPath;
-            this.tokensPath = tokensPath;
-            this.random = new Random();
+            _accountsPath = accountsPath;
+            _tokensPath = tokensPath;
+            _random = new Random();
+            _tokenClearer = null;
 
             //Create files if do not already exist
             using (StreamWriter w = File.AppendText(accountsPath)) ;
@@ -34,7 +38,7 @@ namespace Authenticator
         [MethodImpl(MethodImplOptions.Synchronized)]
         public string Register(string name, string password)
         {
-            string[] accountsFile = File.ReadAllLines(accountsPath);
+            string[] accountsFile = File.ReadAllLines(_accountsPath);
 
             if (name.Contains(" ") || password.Contains(" "))
                 return "Error: username/password cannot contain vertical bar ('|')";
@@ -61,7 +65,7 @@ namespace Authenticator
 
             try
             {
-                using (StreamWriter fileWriter = File.AppendText(accountsPath))
+                using (StreamWriter fileWriter = File.AppendText(_accountsPath))
                 {
                     fileWriter.WriteLine(formattedUserData);
                     fileWriter.Close();
@@ -80,7 +84,7 @@ namespace Authenticator
         public int Login(string name, string password)
         {
             //Search accounts for username/password combo
-            string[] accountsFile = File.ReadAllLines(accountsPath);
+            string[] accountsFile = File.ReadAllLines(_accountsPath);
 
             bool match = false;
             foreach (string line in accountsFile)
@@ -102,12 +106,12 @@ namespace Authenticator
             if (match)
             {
                 //Generate token (making sure it hasn't already been picked)
-                string curTokensString = File.ReadAllText(tokensPath);
+                string curTokensString = File.ReadAllText(_tokensPath);
 
                 bool verified = false;
                 while (!verified)
                 {
-                    token = random.Next();
+                    token = _random.Next();
                     if (!curTokensString.Contains(token.ToString()))
                     {
                         verified = true;
@@ -115,7 +119,7 @@ namespace Authenticator
                 }
 
                 //Write token to file
-                using (StreamWriter tokenFileWriter = File.AppendText(tokensPath))
+                using (StreamWriter tokenFileWriter = File.AppendText(_tokensPath))
                 {
                     tokenFileWriter.WriteLine(token.ToString());
                 } 
@@ -134,7 +138,7 @@ namespace Authenticator
         {
             try
             {
-                string curTokensString = File.ReadAllText(tokensPath);
+                string curTokensString = File.ReadAllText(_tokensPath);
                 if (curTokensString.Contains(token.ToString()))
                 {
                     return "validated";
@@ -150,6 +154,19 @@ namespace Authenticator
             }
         }
 
+        private void InitTokenClearer(int interval)
+        {
+            _tokenClearer = new Timer(ClearTokens, null, interval, interval);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void ClearTokens(object state)
+        {
+            Console.WriteLine("Clearing tokens...");
+            //Overwrite tokens file to completely wipe it
+            File.WriteAllText(_tokensPath, "") ;
+        }
+
         private static void Main(string[] args)
         {
             string accountsPath = "C:/dc-assignment-1/accounts.txt";
@@ -162,12 +179,50 @@ namespace Authenticator
             NetTcpBinding tcp = new NetTcpBinding();
             string url = "net.tcp://0.0.0.0:8101/AuthenticationProvider";
 
+            AuthenticationServer server = new AuthenticationServer(accountsPath, tokensPath);
+
             //Bind service host to server
-            ServiceHost host = new ServiceHost(new AuthenticationServer(accountsPath, tokensPath)); //Bind service host to existing instance, making it a singleton
+            ServiceHost host = new ServiceHost(server); //Bind service host to existing instance, making it a singleton
             host.AddServiceEndpoint(typeof(IAuthenticationServer), tcp, url);
             host.Open();
 
             Console.WriteLine("Server Online.");
+
+            //Read in user input for interval at which tokens should be cleared
+            bool check = false;
+            int interval = -1;
+            while (!check)
+            {
+                Console.Write("Please enter the token clear interval (in seconds): ");
+                //https://stackoverflow.com/questions/6169288/execute-specified-function-every-x-seconds
+
+                try
+                {
+                    interval = int.Parse(Console.ReadLine());
+
+                    if (interval >= 1)
+                    {
+                        check = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a value greater than 1.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid integer.");
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Please enter a valid integer.");
+                }
+            }
+
+            //Create timer to wipe file at given interval
+            server.InitTokenClearer(interval * 1000);
+
+            Console.WriteLine($"Wiping at interval of {interval} seconds.");
 
             Console.ReadLine();
             host.Close();
